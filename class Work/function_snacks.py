e0fd219f2ea70ec7c6d779c4bdcb35afc7bb4450
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-07-30 14:55:10 +0100

    Future Investment Value

--- class Work/function_snacks.py
+++ class Work/function_snacks.py
@@ -5,4 +5,11 @@ def divide_or_square(number):
 	elif number % 5 == 0 : return round(math.sqrt(number),2)
 	else : return number % 5
 
-	
+def cal_future_investment(investment_amount, monthly_rate,amount_of_years):
+	if type(investment_amount) == str or type(monthly_rate) == str or type(amount_of_years) == str :
+		raise ValueError
+	numberOfMonths = 12 * amount_of_years
+	future_investment_value = (investment_amount *(1+monthly_rate))**numberOfMonths
+	return future_investment_value
+		
+		
--- class Work/test_function_snacks.py
+++ class Work/test_function_snacks.py
@@ -1,7 +1,7 @@
 import function_snacks
 import unittest
 
-class TestFuction_function_snacks(unittest.TestCase):
+class TestFunctionSnacks(unittest.TestCase):
 	def test_that_divide_or_square_exits_in_function_snacks(self) :
 		function_snacks.divide_or_square(1)
 
@@ -21,4 +21,27 @@ class TestFuction_function_snacks(unittest.TestCase):
 	def test_that_divide_snacks_takes_in_negative_and_return_valueError(self):
 		self.assertRaises(ValueError,function_snacks.divide_or_square,-1)
 	
+	
+class TestFunctionFutureInvestment(unittest.TestCase):
+	def test_that_future_investment_function_exist(self):
+		function_snacks.cal_future_investment(1,2,3)
+
+	def test_that_cal_future_investment_take_in_incorrect_monthly_value(self):
+		self.assertRaises(ValueError,function_snacks.cal_future_investment,2000,"monthly",30)
+
+	def test_that_cal_future_investment_take_in_incorrect_amount_value(self):
+		self.assertRaises(ValueError,function_snacks.cal_future_investment,"year",10,30)
+
+	def test_that_cal_future_investment_take_in_incorrect_rate_value(self):
+		self.assertRaises(ValueError,function_snacks.cal_future_investment,1000,10,"rate")
+	
+	def test_that_cal_future_investment_returns_correct_result(self):
+		result = function_snacks.cal_future_investment(200,0.05,1)
+		self.assertEqual(result,7.355827511386641e+27)
+	
+	
+	
+		
+	
+		
 	
